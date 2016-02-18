fd749f1529c107a07eb1392ecb93089fe3d90f62
Darayus Nanavati <contact@darayus.com>
2016-02-18 12:06:36 +1100

    Improved the way ToJson is implemented

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,25 @@ extern crate serde_json;
 use serde::ser::Serialize;
 use serde_json::{Value, to_value};
 
-pub trait ToJson: Serialize {
+pub trait ToJson {
+    fn to_json(&self) -> Value;
+}
+
+impl<T> ToJson for T where T: Serialize {
     fn to_json(&self) -> Value {
         to_value(&self) 
     }
 }
 
-impl<T> ToJson for T where T: Serialize {}
-
 #[test]
-fn serde_to_json() {
+fn serde_to_json_str() {
     let my_text = "My Text".to_owned();
     assert_eq!(my_text.to_json(), Value::String(my_text.clone()));
+}
+
+#[test]
+fn serde_to_json_vex() {
+    let my_vec = vec!["This", "text"];
+    let my_vec_expected = vec!["This".to_json(), "text".to_json()];
+    assert_eq!(my_vec.to_json(), Value::Array(my_vec_expected));
 }
